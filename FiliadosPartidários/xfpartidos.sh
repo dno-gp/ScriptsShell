2edbb02ed66813571c4acddf7cb4617d59ccc718
dno-gp <dno.sales@gmail.com>
2021-08-09 16:58:42 -0300

    Update

--- FiliadosPartidários/xfpartidos.sh
+++ FiliadosPartidários/xfpartidos.sh
@@ -6,7 +6,8 @@
 #-----------------------------------------------------------------------------
 #	Este script baixa dados de filiados partidários disponíveis no site do
 #	TSE. Os dados podem ser acessados no link: 	<https://www.tse.jus.br/
-#					hotsites/pesquisas-eleitorais/filiacao_partidaria.htm>
+#	hotsites/pesquisas-eleitorais/filiacao_partidaria.htm>
+#
 #	O script formata, filtra e faz ajustes de codificação nos arquivos
 #	baixados para posterior manipulação em Sistemas Gerenciadores de Banco de
 #	Dados ou linguagem de programação.
@@ -20,7 +21,7 @@ echo -e "Processo iniciado.\n"
 
 mkdir filiados/
 
-#Download dos arquivos no site do tse
+# Download dos arquivos no site do tse
 for x in $(cat partidos.txt); do #Substituir o aquivo txt por uma array/lista
 	wget http://agencia.tse.jus.br/estatistica/sead/eleitorado/filiados/uf/\
 		 filiados_${x}_pb.zip
@@ -33,7 +34,7 @@ for x in $(cat partidos.txt); do #Substituir o aquivo txt por uma array/lista
 	rm -r *.csv	
 done
 
-#Filtra arquivos.
+# Filtra arquivos.
 cd filiados/ ; pwd
 
 touch auxiliar.txt
@@ -53,7 +54,7 @@ mv partidos_ms.txt ../
 cd ../
 rm -r filiados/
 
-#Ajusta colunas 
+# Ajusta colunas 
 cut -d";" -f1-5,8-19 partidos_ms.txt > filiados_mrs.txt; rm -r partidos_ms.txt
 
 
