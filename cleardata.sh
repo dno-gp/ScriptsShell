48243bbafa506d1b02b31e0f4f684a2590ff11a9
dno-gp <dno.sales@gmail.com>
2021-08-13 15:45:52 -0300

    Update

--- cleardata.sh
+++ cleardata.sh
@@ -2,26 +2,22 @@
 #
 # dataclear.sh - Faz ajustes de formatação em arquivos contendo dados.
 #
-# Site		:
 # Autor		:Edson Sales <sales_eds@hotmail.com>
-# Manutenção	:
 #
-#---------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 # Este programa recebe como parâmetro o nome do arquivo,
 # do qual é gerado uma cópia com as alterações estabelecidas.
 #
 #	Exemplo:
 #		$ ./cleatdata.sh arquivo
 #
-#---------------------------------------------------------------------------
-# 
-#
-# Histórico:
-#
+#-----------------------------------------------------------------------------
 #
 # Licença: GPL
-#---------------------------------------------------------------------------
-echo "codificação do arquivo:"; file $1 #TODO Estes dois comandos devem ser exibidos na mesma linha no momento da execução.
-sed -i '1s/^.*$/\L&/g' $1	# Converte cabeçalho (1ªlinha) para letras minúsculas.
+#
+#######################################################################CÓDIGO:
 
-###########################################################################FIM
+echo "codificação do arquivo: "; file $1 #TODO exibir/print em mesma linha.
+sed -i '1s/^.*$/\L&/g' $1 # Converte cabeçalho(Linha 1)para letras minúsculas.
+
+##########################################################################FIM.
